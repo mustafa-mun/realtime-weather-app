71992d0772866f17fca57ca4acfaa2ef8dfd2142
Mustafa <mustafamcm9@gmail.com>
2023-03-09 18:02:02 +0300

    base logic

--- script.js
+++ script.js
@@ -1,18 +1,111 @@
-let searchKey = "";
-searchKey = "aksaray";
+const elements = (() => {
+  const searchBtn = document.getElementById("search-btn");
+  const inputEl = document.querySelector("input");
+  const errorMsg = document.getElementById("error-msg");
+  const body = document.querySelector("body");
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchKey}&APPID=babd2876d8bd18cc2edf5ff02fad870f`;
+  return {
+    inputEl,
+    searchBtn,
+    errorMsg,
+    body,
+  };
+})();
 
-function fToCTempConversion(f) {
-  return Math.round((f - 32) * .5556)
+const variables = (() => {
+  const searchKey = "";
+  const letterRegex = /[A-Za-z]/;
+
+  return {
+    searchKey,
+    letterRegex,
+  };
+})();
+
+const dom = (() => {
+  const forecastDom = (cityName, temp, feelsLike, humidity, wind) => {
+    const main = document.createElement("main");
+    const cityNameText = document.createElement("p");
+    const tempText = document.createElement("p");
+    const feelsLikeText = document.createElement("p");
+    const humidityText = document.createElement("p");
+    const windText = document.createElement("p");
+
+    main.className = "forecast-container";
+    main.id = "forecast";
+
+    cityNameText.className = "xl-text";
+    cityNameText.innerHTML = cityName;
+
+    tempText.className = "big-text";
+    tempText.innerHTML = `${temp} &degC`;
+
+    feelsLikeText.innerHTML = `Feels like: ${feelsLike} &degC`;
+    humidityText.innerHTML = `Humidity: ${humidity}%`;
+    windText.innerHTML = `Wind: ${wind} km/h`;
+
+    main.appendChild(cityNameText);
+    main.appendChild(tempText);
+    main.appendChild(feelsLikeText);
+    main.appendChild(humidityText);
+    main.appendChild(windText);
+
+    return main;
+  };
+
+  return {
+    forecastDom,
+  };
+})();
+
+function kToCTempConversion(k) {
+  // Convert kelvin to celcius
+  return Math.round(k - 273.15);
 }
 
-console.log(fToCTempConversion(50));
+async function fetchWeatherApi(url) {
+  try {
+    const fetchApi = await fetch(url);
+    const response = await fetchApi.json();
 
-async function fetchWeatherApi() {
-  const fetchApi = await fetch(url)
-  const response = await fetchApi.json()
-  console.log(response);
+    const forecastContainer = document.getElementById("forecast");
+
+    if (!forecastContainer) {
+      elements.body.appendChild(
+        dom.forecastDom(
+          response.name,
+          kToCTempConversion(response.main.temp),
+          kToCTempConversion(response.main.feels_like),
+          response.main.humidity,
+          response.wind.speed
+        )
+      );
+    } else {
+      elements.body.removeChild(elements.body.lastChild);
+      elements.body.appendChild(
+        dom.forecastDom(
+          response.name,
+          kToCTempConversion(response.main.temp),
+          kToCTempConversion(response.main.feels_like),
+          response.main.humidity,
+          response.wind.speed
+        )
+      );
+    }
+  } catch (err) {
+    elements.errorMsg.textContent = err;
+  }
 }
 
-fetchWeatherApi()
+elements.searchBtn.addEventListener("click", () => {
+  const inputValue = elements.inputEl.value;
+
+  if (inputValue && variables.letterRegex.test(inputValue)) {
+    variables.searchKey = inputValue;
+    elements.errorMsg.textContent = "";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${variables.searchKey}&APPID=babd2876d8bd18cc2edf5ff02fad870f`;
+    fetchWeatherApi(url);
+  } else {
+    elements.errorMsg.textContent = "Input is Invalid!";
+  }
+});
